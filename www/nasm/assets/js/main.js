c08e8ca95a746b6576a3f7a19c62e794fcef407c
Tony Porto <Tony.Porto@ascendlearning.com>
2016-07-30 03:49:59 -0500

    no message

--- www/nasm/assets/js/main.js
+++ www/nasm/assets/js/main.js
@@ -102,7 +102,7 @@ function wcagKeyboardEvents() {
 		aria_hidden = {'aria-expanded': 'false'};
 
 	
-	
+	jQuery(".navbar-bottom li:not(.dropdown) > a").on('click.dropdown.data-api', clearMenus)
 	
 	//link_clicker = {'class': 'dropdown-toggle', 'data-toggle': 'dropdown'};
 	//jQuery(".navbar-bottom > li:has(ul)").find("a:first").attr(link_clicker)
