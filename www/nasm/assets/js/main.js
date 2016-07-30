bff7047e3e8752684830c02acdc729c77b194a85
Tony Porto <Tony.Porto@ascendlearning.com>
2016-07-30 03:39:44 -0500

    no message

--- www/nasm/assets/js/main.js
+++ www/nasm/assets/js/main.js
@@ -337,7 +337,7 @@ jQuery(document).on('click.nav','.navbar-collapse.in',function(e) {
  * ================================== */
 jQuery(document).ready(function() {
 	
-jQuery(".navbar-bottom li:not(.dropdown) > a").on("click", function (e) {
+jQuery(".navbar-bottom li:not(.dropdown) > a").on("click touchend", function (e) {
 	e.preventDefault();
     window.location = jQuery(this).attr("href");
 
