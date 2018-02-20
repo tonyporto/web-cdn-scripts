c165089f5259c24912e98075ddb96e2916fd478d
Tony Porto <tony.porto@ascendlearning.com>
2018-02-20 15:21:52 -0600

    no message

--- scripts/jsoneditor/loader.js
+++ scripts/jsoneditor/loader.js
@@ -27,20 +27,7 @@ loadjs([
 	'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'], 'jsoneditor', {
   success: function() {
 
-		var container = document.getElementById("jsoneditor");
-				
-		var options = {
-				mode: 'tree',
-				modes: ['code', 'form', 'text', 'tree', 'view'], // allowed modes
-				onError: function (err) {
-					alert(err.toString());
-				},
-				onModeChange: function (newMode, oldMode) {
-					console.log('Mode switched from', oldMode, 'to', newMode);
-				},
-		};
 
-		var editor = new JSONEditor(container, options, json);
 
 		function fileFetcher(file, callback) {
 			var rawFile = new XMLHttpRequest();
