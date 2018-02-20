c5e77f4d929e19c99c5a69fe059b03f4ff9ed52b
Tony Porto <tony.porto@ascendlearning.com>
2018-02-20 15:10:27 -0600

    no message

--- scripts/jsoneditor/loader.js
+++ scripts/jsoneditor/loader.js
@@ -24,7 +24,8 @@ loadjs([
 	editorDir + 'jsoneditor.js',
 	editorDir + 'filereader.js',
 	editorDir + 'FileSaver.min.js',
-	'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'], 'jsoneditor', {
+	'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js',
+	editorDir + 'loadAndSave.js'], 'jsoneditor', {
   success: function() {
 
 		var container = document.getElementById("jsoneditor");
