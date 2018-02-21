704b663d209e8f65e520c8cdd53085a5cb1837ee
Tony Porto <tony.porto@ascendlearning.com>
2018-02-20 23:03:21 -0600

    no message

--- scripts/jsoneditor/loader.js
+++ scripts/jsoneditor/loader.js
@@ -58,6 +58,18 @@ editor.set(json);
 
 }
 
+function fileFetcher(file, callback) {
+	var rawFile = new XMLHttpRequest();
+		rawFile.overrideMimeType("application/json");
+		rawFile.open("GET", file, true);
+		rawFile.onload = function() {
+			if (rawFile.readyState === 4 && rawFile.status == "200") {
+				callback(rawFile.responseText);
+			}
+	}
+	rawFile.send();
+}
+
 	// Load a JSON document
 	if (document.getElementById('loadDocument')) {
 		FileReaderJS.setupInput(document.getElementById('loadDocument'), {
