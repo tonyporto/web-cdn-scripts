156edf4e21fb8c582fc6445280067dfe5b650af6
Tony Porto <tony.porto@ascendlearning.com>
2018-02-20 18:29:45 -0600

    no message

--- scripts/jsoneditor/loader.js
+++ scripts/jsoneditor/loader.js
@@ -77,15 +77,13 @@ function fileFetcher(file, callback) {
 	var rawFile = new XMLHttpRequest();
 		rawFile.overrideMimeType("application/json");
 		rawFile.open("GET", file, true);
-		rawFile.onreadystatechange = function() {
+		rawFile.onload = function() {
 			if (rawFile.readyState === 4 && rawFile.status == "200") {
-
 				callback(rawFile.responseText);
-
 			}
 	}
-	rawFile.send(null);
-}	
+	rawFile.send();
+}
  
   
   },
